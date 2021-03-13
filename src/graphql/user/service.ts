36055f8c7721f334cdb8e3b1de16ba1fee52ed79
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-14 04:13:13 +0700

    check obj is empty before execute

--- src/graphql/user/service.ts
+++ src/graphql/user/service.ts
@@ -1,10 +1,16 @@
+import errorHandler from '../../handler/error'
+
 import UserModel, { ICreateUser, IUser } from '../../models/user'
+
+import { ARGUMENT_IS_REQUIRED } from '../../constants/errors/args'
+import { isObjectEmpty } from '../../utils/validate'
 
 async function findAll(): Promise<IUser[]> {
   return await UserModel.find({})
 }
 
 async function addUser(input: ICreateUser): Promise<IUser> {
+  if (isObjectEmpty<ICreateUser>(input)) errorHandler(ARGUMENT_IS_REQUIRED.message, ARGUMENT_IS_REQUIRED.code)
   const newUser = new UserModel({
     ...input,
   })
