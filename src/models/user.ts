43d1f4270dd7a4554dbc5905976e0f4973b5b254
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 04:01:49 +0700

    move token to auth service interface

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,16 +2,14 @@ import { ApolloError } from 'apollo-server-errors'
 import * as mongoose from 'mongoose'
 
 import { NextError, Error } from '@src/types'
-
-interface Token {
-  accessToken: string
-  refreshToken: string
-}
+import { EMAIL_IS_ALREADY_EXIST } from '@src/constants/errors/user'
+import { Token } from '@src/graphql/auth'
 
 export interface IUser extends mongoose.Document {
   firstName: string
   lastName: string
   email: string
+  password: string
   token: Token
 }
 
@@ -22,13 +20,18 @@ export interface ICreateUser {
   password: string
 }
 
+const TokenSchema = {
+  accessToken: { type: String },
+  refreshToken: { type: String },
+}
+
 const UserSchema: mongoose.Schema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    token: { type: String },
+    token: { type: TokenSchema },
   },
   {
     timestamps: true,
@@ -41,7 +44,7 @@ UserSchema.pre('save', (next: NextError) => {
 
 UserSchema.post('save', (error: Error, _: unknown, next: NextError) => {
   if (error.code === 11000) {
-    next(new ApolloError('Duplicate email. Try another.'))
+    next(new ApolloError(EMAIL_IS_ALREADY_EXIST.message, EMAIL_IS_ALREADY_EXIST.code))
   } else {
     next(error)
   }
