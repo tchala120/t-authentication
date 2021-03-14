7a7b0ab2535663e610fb9161a2ee6a78db55492e
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 04:02:08 +0700

    sign token with access and refresh token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,7 +1,11 @@
 import jwt from 'jsonwebtoken'
+import { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } from '@constants/environment'
 
-import { TOKEN_NOT_BEARER_TYPE } from 'src/constants/errors/auth'
-import errorHandler from 'src/handler/error'
+import { TOKEN_NOT_BEARER_TYPE } from '@constants/errors/auth'
+import errorHandler from '@handler/error'
+import { Token } from '@src/graphql/auth'
+
+type SignTokenData<T> = T | string | number
 
 export interface TokenResult {
   payload: any
@@ -16,4 +20,14 @@ export function checkToken(token: string): any {
   const isTokenVerify = jwt.verify(token, 'hello')
 
   return isTokenVerify
+}
+
+export function signToken<T>(data: SignTokenData<T>): Token {
+  const accessToken = jwt.sign(JSON.stringify(data), ACCESS_TOKEN_SECRET || '')
+  const refreshToken = jwt.sign(JSON.stringify(data), REFRESH_TOKEN_SECRET || '')
+
+  return {
+    accessToken,
+    refreshToken,
+  }
 }
