845135053484b9fa1739bdf00f420ce8c62d4b42
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 04:24:35 +0700

    remove withAuth middleware for authentication mutation

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -5,8 +5,6 @@ import authService from '@graphql/auth/service'
 
 import { Context } from '@src/index'
 
-import withAuth from '@middlewares/withAuth'
-
 import { IUser } from '@models/user'
 
 import { Argument } from '@src/types'
@@ -27,7 +25,7 @@ interface Resolvers extends IResolvers {
 export const resolvers: Resolvers = {
   Query: {},
   Mutation: {
-    login: async (...args): Promise<IUser> => withAuth<IUser>(...args)(await authService.login(args[1].input)),
+    login: async (...args): Promise<IUser> => await authService.login(args[1].input),
     register: async (...args): Promise<IUser> => await authService.register(args[1].input),
   },
 }
