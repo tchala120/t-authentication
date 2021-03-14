398981c7aabc99ab26e64cd8fe840d8d7b0a012e
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 02:08:18 +0700

    add type of login input and register input

---
+++ src/graphql/auth/interface.ts
@@ -0,0 +1,11 @@
+export interface ILoginInput {
+  email: string
+  password: string
+}
+
+export interface IRegisterInput {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+}
