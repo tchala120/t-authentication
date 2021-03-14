98cc49021cc0de71e85995011f59448141226e98
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 05:43:47 +0700

    add error about email not found

--- src/constants/errors/user.ts
+++ src/constants/errors/user.ts
@@ -4,3 +4,13 @@ export const EMAIL_IS_ALREADY_EXIST: ErrorPayload = {
   message: 'This email is already exist. Try another.',
   code: 'EMAIL_ALREADY_EXIST',
 }
+
+export const EMAIL_NOT_FOUND: ErrorPayload = {
+  message: 'This email not found. Please register first.',
+  code: 'EMAIL_NOT_FOUND',
+}
+
+export const EMAIL_OR_PASSWORD_NOT_CORRECT: ErrorPayload = {
+  message: 'Email or password not correct.',
+  code: 'EMAIL_OR_PASSWORD_NOT_CORRECT',
+}
