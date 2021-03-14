83322874683a12144526366c099159159ab0c72f
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 02:08:35 +0700

    add password type in mongoose schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -26,6 +26,7 @@ const UserSchema: mongoose.Schema = new mongoose.Schema(
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     token: { type: String },
   },
   {
