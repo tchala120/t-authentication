5f9cf4480ba840813be5b4d2c31415e1262567d0
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 05:44:20 +0700

    function hashing password

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,5 +1,11 @@
-import { compare } from 'bcryptjs'
+import { compare, hash } from 'bcryptjs'
+
+import { SALT } from '@constants/environment'
 
 export async function isPasswordCorrect(password: string, hashPassword: string) {
   return (await compare(password, hashPassword)).valueOf
+}
+
+export async function hashingPassword(password: string): Promise<string> {
+  return await hash(password, SALT)
 }
