a4d88176f0cd3b69642c44abf94be744a2e53b6c
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 05:47:16 +0700

    remove salt env

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -9,6 +9,3 @@ export const USERNAME = process.env.DB_USERNAME
 export const PASSWORD = process.env.DB_PASSWORD
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET
-export const SALT =
-  process.env.SALT ||
-  '20bf666f19107c20452be7c7ba5d0098c09c8567373eb11a821be8abf4c1c0040f8b662245a56daf2fd0625ab03c7af581633858cd3fa07d7c67a286c5789858'
