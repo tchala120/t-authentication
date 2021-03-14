0a664464f4ebc11cb6ca6e77fcdf9093b0e8b668
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-14 22:23:52 +0700

    add error code for token type is not bearer

--- src/constants/errors/auth.ts
+++ src/constants/errors/auth.ts
@@ -9,3 +9,8 @@ export const TOKEN_EXPIRED: ErrorPayload = {
   message: 'This token is already expired.',
   code: 'TOKEN_EXPIRED',
 }
+
+export const TOKEN_NOT_BEARER_TYPE: ErrorPayload = {
+  message: 'This token is not bearer type, please re-check',
+  code: 'TOKEN_NOT_BEARER_TYPE',
+}
