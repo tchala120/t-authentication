4441fe1e6fba1d831600270b100c5ce231600c60
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 03:17:25 +0700

    fixed env not load

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv'
+import path from 'path'
+import { config } from 'dotenv'
 
-dotenv.config({
-  path: `.env.${process.env.NODE_ENV || 'local'}`,
+config({
+  path: path.join(__dirname, '../../', `.env.${process.env.NODE_ENV || 'local'}`),
 })
 
 export const USERNAME = process.env.DB_USERNAME
 export const PASSWORD = process.env.DB_PASSWORD
-
 export const SECRET_KEY = process.env.SECRET_KEY
--- src/database.ts
+++ src/database.ts
@@ -1,25 +1,21 @@
 import mongoose from 'mongoose'
-import { PASSWORD, USERNAME } from './constants/environment'
+
+import { PASSWORD, USERNAME } from '@constants/environment'
 
 function setupDatabase(): void {
-  const username = USERNAME
-  const password = PASSWORD
-  if (username && password) {
-    const uri = `mongodb+srv://${username}:${password}@cluster0.eignu.mongodb.net/tcore-dev?retryWrites=true&w=majority`
-    mongoose.Promise = global.Promise
-    mongoose
-      .connect(uri, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useFindAndModify: false,
-      })
-      .then(() => console.log('Database connected'))
-      .catch((e: Error) => {
-        throw e.message
-      })
-  } else {
-    throw Error('Cannot connect to database cause of have no data access.')
-  }
+  const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.eignu.mongodb.net/tcore-dev?retryWrites=true&w=majority`
+  mongoose.Promise = global.Promise
+  mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('Database connected'))
+    .catch((e: Error) => {
+      throw e.message
+    })
 }
 
 export default setupDatabase
