37f7da9694c5547787396010e4a1029aa0ffb598
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 22:45:09 +0700

    add type of Record for error variable

--- src/handler/error.ts
+++ src/handler/error.ts
@@ -2,7 +2,7 @@ import { ApolloError } from 'apollo-server-errors'
 
 import type { IErrorPayload } from '@constants/errors'
 
-function errorHandler(error: IErrorPayload): void {
+function errorHandler(error: IErrorPayload | Record<string, any>): void {
   throw new ApolloError(error.message, error.code)
 }
 
