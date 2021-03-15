38d085fa0e6ebf348a63199435467ce9a94740fb
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 07:45:38 +0700

    token handler expired and verify

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,25 +1,44 @@
 import jwt from 'jsonwebtoken'
+import dayjs from 'dayjs'
 
+import { SIGNING_TOKEN_OCCURED, TOKEN_NOT_BEARER_TYPE } from '@constants/errors/auth'
 import { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } from '@constants/environment'
-import { SIGNING_TOKEN_OCCURED } from '@constants/errors/auth'
+import { TOKEN_TYPE } from '@src/constants/token'
 
 import errorHandler from '@handler/error'
 
 import { Token } from '@src/graphql/auth'
 
+import { JwtResult, UserJwt } from '@src/types'
+
 type SignTokenData<T> = T | string
 
-export function tokenDecode(token: string): any {
-  const decoded = jwt.decode(token)
-  return decoded
+function filterTokenType(token: string): string {
+  return token.substring(TOKEN_TYPE.length)
 }
 
-export function tokenVerify(token: string): any {
-  return jwt.verify(token, ACCESS_TOKEN_SECRET || '')
+export function tokenVerify(token: string): UserJwt {
+  if (!isTokenBearerType(token)) throw errorHandler(TOKEN_NOT_BEARER_TYPE)
+
+  token = filterTokenType(token)
+
+  return jwt.verify(token, ACCESS_TOKEN_SECRET || '') as UserJwt
+}
+
+export function isTokenExpired(token: string): boolean {
+  token = filterTokenType(token)
+
+  const userJWT: JwtResult = jwt.decode(token, { complete: true }) as JwtResult
+
+  return dayjs(userJWT.payload.exp * 1000).isBefore(dayjs())
 }
 
 export function isTokenVerify(token: string): boolean {
   return !!tokenVerify(token)
+}
+
+export function isTokenBearerType(token: string): boolean {
+  return token.startsWith('Bearer')
 }
 
 export function signToken<T extends Record<string, any>>(data: SignTokenData<T>): Token {
