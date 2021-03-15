accbbb6bbbdec7aadb5a41f1128aa7a2de8c84c6
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 07:05:22 +0700

    moved token verify check here

--- src/middlewares/withAuth.ts
+++ src/middlewares/withAuth.ts
@@ -1,5 +1,7 @@
 import { AuthenticationError } from 'apollo-server-errors'
 
+import { isTokenVerify } from '@utils/token'
+
 type AuthenticationResult<T> = (wrapper: T) => T
 
 function withAuth<T>(...args: any[]): AuthenticationResult<T> {
@@ -8,6 +10,8 @@ function withAuth<T>(...args: any[]): AuthenticationResult<T> {
   const { isLoggedIn, token } = ctx.auth
 
   if (!isLoggedIn || !token) throw new AuthenticationError('Must Authenticate')
+
+  if (isTokenVerify(token)) throw new AuthenticationError('Token is not verified')
 
   return (wrapper: T): T => {
     return wrapper
