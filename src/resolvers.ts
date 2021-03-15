360b3259d11497b1f0b7c8ce882307c30f70c48c
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 07:03:26 +0700

    add me for get profile

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -2,12 +2,14 @@ import { GraphQLResolveInfo } from 'graphql'
 import { IResolvers } from 'graphql-tools'
 
 import authService from '@graphql/auth/service'
+import userService from '@graphql/user/service'
 
 import { Context } from '@src/index'
 
 import { IUser } from '@models/user'
 
 import { Argument } from '@src/types'
+import withAuth from './middlewares/withAuth'
 
 type ResolverOptions = (
   parent: ParentNode,
@@ -23,7 +25,9 @@ interface Resolvers extends IResolvers {
 }
 
 export const resolvers: Resolvers = {
-  Query: {},
+  Query: {
+    me: async (...args): Promise<IUser> => withAuth<IUser>(...args)(await userService.me(args[2])),
+  },
   Mutation: {
     login: async (...args): Promise<IUser> => await authService.login(args[1].input),
     register: async (...args): Promise<IUser> => await authService.register(args[1].input),
