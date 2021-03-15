2066fa2bb9cf5b905537bfb84794081073586972
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 07:45:04 +0700

    check token expired

--- src/middlewares/withAuth.ts
+++ src/middlewares/withAuth.ts
@@ -1,17 +1,19 @@
 import { AuthenticationError } from 'apollo-server-errors'
 
-import { isTokenVerify } from '@utils/token'
+import { isTokenExpired, isTokenVerify } from '@utils/token'
 
 type AuthenticationResult<T> = (wrapper: T) => T
 
 function withAuth<T>(...args: any[]): AuthenticationResult<T> {
   const ctx = args[2]
 
-  const { isLoggedIn, token } = ctx.auth
+  const { token } = ctx.auth
 
-  if (!isLoggedIn || !token) throw new AuthenticationError('Must Authenticate')
+  if (!token) throw new AuthenticationError('Must Authenticate')
 
-  if (isTokenVerify(token)) throw new AuthenticationError('Token is not verified')
+  if (isTokenExpired(token)) throw new AuthenticationError('Token is expired')
+
+  if (!isTokenVerify(token)) throw new AuthenticationError('Token is not verified')
 
   return (wrapper: T): T => {
     return wrapper
