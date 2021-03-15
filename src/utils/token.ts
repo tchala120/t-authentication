21e5d3d6398df5c3732cb8ec3a1cc2cd8e0f4b66
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-15 15:23:16 +0700

    Rename interface start with I

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -7,9 +7,9 @@ import { TOKEN_TYPE } from '@src/constants/token'
 
 import errorHandler from '@handler/error'
 
-import { Token } from '@src/graphql/auth'
+import { IToken } from '@src/graphql/auth'
 
-import { JwtResult, UserJwt } from '@src/types'
+import { IJwtResult, UserJwt } from '@src/types'
 
 type SignTokenData<T> = T | string
 
@@ -28,7 +28,7 @@ export function tokenVerify(token: string): UserJwt {
 export function isTokenExpired(token: string): boolean {
   token = filterTokenType(token)
 
-  const userJWT: JwtResult = jwt.decode(token, { complete: true }) as JwtResult
+  const userJWT: IJwtResult = jwt.decode(token, { complete: true }) as IJwtResult
 
   return dayjs(userJWT.payload.exp * 1000).isBefore(dayjs())
 }
@@ -41,7 +41,7 @@ export function isTokenBearerType(token: string): boolean {
   return token.startsWith('Bearer')
 }
 
-export function signToken<T extends Record<string, any>>(data: SignTokenData<T>): Token {
+export function signToken<T extends Record<string, any>>(data: SignTokenData<T>): IToken {
   try {
     const accessToken = jwt.sign(data, ACCESS_TOKEN_SECRET || '', { expiresIn: '3m' })
     const refreshToken = jwt.sign(data, REFRESH_TOKEN_SECRET || '', { expiresIn: '7d' })
