299e9093386a3b61a1423e5a7a49d4cd3d092928
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-17 01:50:24 +0700

    moved session to middleware transaction

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,6 @@
-import mongoose, { ClientSession } from 'mongoose'
+import mongoose from 'mongoose'
 
 import { PASSWORD, USERNAME } from '@constants/environment'
-
-export let session: ClientSession
 
 function setupDatabase(): void {
   const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.eignu.mongodb.net/tcore-dev?retryWrites=true&w=majority`
@@ -15,12 +13,7 @@ function setupDatabase(): void {
       useCreateIndex: true,
       useUnifiedTopology: true,
     })
-    .then(async (res) => {
-      console.log('Database connected')
-      session = await res.startSession()
-
-      session.startTransaction()
-    })
+    .then(() => console.log('Database connected'))
     .catch((e: Error) => {
       throw e.message
     })
