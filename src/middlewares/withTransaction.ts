7ffd016cd4046753edc7a6c4881155fa797b6d0c
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-17 02:14:00 +0700

    change to await for prevent callback hell

--- src/middlewares/withTransaction.ts
+++ src/middlewares/withTransaction.ts
@@ -1,6 +1,5 @@
 import { EMAIL_IS_ALREADY_EXIST } from '@src/constants/errors/user'
 import errorHandler from '@src/handler/error'
-import { IError } from '@src/types'
 import mongoose, { ClientSession } from 'mongoose'
 
 import type { WrapperResultSync } from './type'
@@ -8,34 +7,31 @@ import type { WrapperResultSync } from './type'
 type TransactionResult<T> = (wrapper: WrapperResultSync<T>) => Promise<T>
 
 function withTransaction<T>(...args: any[]): TransactionResult<T> {
-  let writeResults: Promise<T>
+  let writeResults: T
 
-  const onTransaction = (session: ClientSession, fn: WrapperResultSync<T>): Promise<void> => {
-    session.startTransaction()
+  const onTransaction = async (session: ClientSession, fn: WrapperResultSync<T>): Promise<void> => {
+    try {
+      const results: T = await fn(...args, session)
+      writeResults = results
 
-    return fn(...args, session)
-      .then(async (results: any) => {
-        writeResults = results
-      })
-      .catch(async (error: IError) => {
-        await session.abortTransaction()
+      session.endSession()
+    } catch (error) {
+      await session.abortTransaction()
+      session.endSession()
 
-        if (error.code === 11000) {
-          throw errorHandler(EMAIL_IS_ALREADY_EXIST)
-        } else {
-          console.log('Error code', error.code)
-          throw errorHandler(error)
-        }
-      })
-      .finally(() => {
-        session.endSession()
-      })
+      if (error.code === 11000) {
+        throw errorHandler(EMAIL_IS_ALREADY_EXIST)
+      } else {
+        console.log('Error code', error.code)
+        throw errorHandler(error)
+      }
+    }
   }
 
   return async (wrapper: WrapperResultSync<T>) => {
     const useTransaction = await mongoose.startSession()
 
-    await useTransaction.withTransaction((session: ClientSession) => onTransaction(session, wrapper))
+    await useTransaction.withTransaction(async (session: ClientSession) => await onTransaction(session, wrapper))
 
     return writeResults
   }
