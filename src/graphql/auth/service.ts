cbc9ecd3b90fb7687a6607be120ea2d2ab93dbc9
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-19 00:53:56 +0700

    service for request access token

--- src/graphql/auth/service.ts
+++ src/graphql/auth/service.ts
@@ -1,9 +1,9 @@
 import type { ClientSession } from 'mongoose'
 import type { ILoginInput, ITokenSign } from '.'
-import type { IRefreshTokenInput, IRegisterInput } from './interface'
+import type { IRefreshTokenInput, IRegisterInput, IToken } from './interface'
 import type { IUserSchema } from '@models/user'
-import type { ITokenSchema } from '@models/token'
 
+import { TOKEN_NOT_FOUND, REFRESH_TOKEN_EXPIRED } from '@src/constants/errors/auth'
 import { ARGUMENT_IS_REQUIRED } from '@constants/errors/args'
 import { EMAIL_NOT_FOUND, EMAIL_OR_PASSWORD_NOT_CORRECT } from '@src/constants/errors/user'
 
@@ -13,9 +13,8 @@ import TokenModel from '@models/token'
 import UserModel from '@models/user'
 
 import { hashingPassword, isPasswordCorrect } from '@src/utils/crypto'
-import { signToken } from '@src/utils/token'
+import { isTokenExpired, saveRefreshTokenToDB, signToken, tokenWithBearer } from '@src/utils/token'
 import { isObjectEmpty } from '@utils/validate'
-import { TOKEN_NOT_FOUND } from '@src/constants/errors/auth'
 
 async function register(input: IRegisterInput, session: ClientSession): Promise<IUserSchema> {
   if (isObjectEmpty(input)) throw errorHandler(ARGUMENT_IS_REQUIRED)
@@ -33,7 +32,7 @@ async function register(input: IRegisterInput, session: ClientSession): Promise<
   return newUser
 }
 
-async function login(input: ILoginInput): Promise<IUserSchema> {
+async function login(input: ILoginInput, session: ClientSession): Promise<IUserSchema> {
   const user = await UserModel.findOne({ email: input.email })
 
   if (!user) throw errorHandler(EMAIL_NOT_FOUND)
@@ -44,19 +43,29 @@ async function login(input: ILoginInput): Promise<IUserSchema> {
     id: user._id,
   })
 
+  await saveRefreshTokenToDB(token.refreshToken)
+
   user.token = token
+
+  await session.commitTransaction()
 
   return user
 }
 
-async function requestAccessToken(input: IRefreshTokenInput): Promise<ITokenSchema> {
-  const token = await TokenModel.findOne({ refreshToken: input.refreshToken })
+async function requestAccessToken(input: IRefreshTokenInput): Promise<IToken> {
+  const token = await TokenModel.findOne({ refreshToken: input.refreshToken }).populate('users')
 
   if (!token) {
     throw errorHandler(TOKEN_NOT_FOUND)
   }
 
-  return token
+  if (isTokenExpired(tokenWithBearer(token.refreshToken))) throw errorHandler(REFRESH_TOKEN_EXPIRED)
+
+  const newToken = signToken<ITokenSign>({ id: token.userId })
+
+  await saveRefreshTokenToDB(newToken.refreshToken)
+
+  return newToken
 }
 
 export default {
