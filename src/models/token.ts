52857d253515355ac2257c3e0c188e4118995e18
Panupong Tipjoi <panupongza_155@hotmail.com>
2021-03-18 23:13:20 +0700

    new token model with expire date time with number

--- src/models/token.ts
+++ src/models/token.ts
@@ -2,16 +2,17 @@ import * as mongoose from 'mongoose'
 
 import type { NextFunction } from '@src/types'
 
-interface ITokenSchema extends mongoose.Document {
-  accessToken: string
-  refresh: string
+export interface ITokenSchema extends mongoose.Document {
+  id: string
+  refreshToken: string
+  expiresIn: number
 }
 
 const TokenSchema: mongoose.Schema = new mongoose.Schema(
   {
     id: { type: mongoose.SchemaTypes.ObjectId },
-    accessToken: { type: String },
     refreshToken: { type: String },
+    expiresIn: { type: Number },
   },
   {
     timestamps: true,
